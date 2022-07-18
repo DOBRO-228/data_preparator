6cb47b3dda23a81d5d7ccbc98c6c7a9555c85fd7
Dobro <matematikaigor@gmail.com>
2022-07-18 16:21:38 +0300

    change logic of covertion date columns. And change relative imports to local folder imports

--- data_preparator/data_preparator.py
+++ data_preparator/data_preparator.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pandas as pd
-from data_preparator import constants
-from data_preparator.data_frame_separators import (
+from . import constants
+from .data_frame_separators import (
     separate_drugs,
     separate_medical_devices,
     separate_rows_with_empty_cells_in_required_columns,
@@ -10,7 +11,7 @@ preprocessed_data_frames = []
 
 
 def process_data_frame(df):
-
+    """Обрабатывает данные."""
     drop_not_required_columns(df)
     rename_columns(df)
     set_uniq_values_in_record_id_column(df)
@@ -67,9 +68,22 @@ def change_commas_to_dots_in_float_columns(df):
         df[column] = df[column].astype(float)
 
 
+def standardize_date_format(date_with_time):
+    """Приводит дату к одному виду."""
+    if date_with_time == '?':
+        return np.nan
+    date_with_time = str(date_with_time)
+    if '/' in date_with_time:
+        return date_with_time
+    date, _ = date_with_time.split(' ')
+    year, month, day = date.split('-')
+    return '{day}/{month}/{year}'.format(day=day, month=month, year=year)
+
+
 def convert_date_columns_to_datetime_format(df):
     """Приводит к одному формату дат колонки с датами."""
     for column in constants.COLUMNS_WITH_DATES:
+        df[column] = df[column].apply(standardize_date_format)
         df[column] = pd.to_datetime(df[column], format='%d/%m/%Y', errors='coerce')
 
 
