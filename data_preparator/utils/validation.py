296fc79234fb088e011bd7608f4be2796096ea9f
Dobro <matematikaigor@gmail.com>
2022-10-31 17:09:29 +0600

    update error column headers. Now input names are returned

--- data_preparator/utils/validation.py
+++ data_preparator/utils/validation.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pydantic import ValidationError
 
+from ..constants import COLUMNS_MAPPING
+
 
 def get_indices_and_info_from_errors(errors: ValidationError) -> dict:
     """Забирает индексы строк и информацию об ошибках.
@@ -24,9 +26,17 @@ def get_indices_and_info_from_errors(errors: ValidationError) -> dict:
 
 def insert_row_errors_info_into_df_by_index(df: pd.DataFrame, indices_of_rows_with_invalid_data: dict) -> None:
     df['ERRORS'] = ''
+    reversed_columns_mapping = {
+        output_column_header: input_column_header
+        for input_column_header, output_column_header in COLUMNS_MAPPING.items()
+    }
     for df_index, errors in indices_of_rows_with_invalid_data.items():
         for error in errors:
             cell_with_row_errors = df.loc[df_index, 'ERRORS']
+            error = {
+                reversed_columns_mapping[column_with_error]: error_message
+                for column_with_error, error_message in error.items()
+            }
             if cell_with_row_errors == '':
                 df.at[df_index, 'ERRORS'] = [error]
             else:
