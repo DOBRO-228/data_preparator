a500de25b050a8f4f6979aa8bd3e743e07b71f26
Dobro <matematikaigor@gmail.com>
2022-12-19 14:08:20 +0300

    change logic of separation. Separation by NPHIES CODES happens firstly and then separation by PRODUCT TYPE

--- data_preparator/data_frame_separators.py
+++ data_preparator/data_frame_separators.py
@@ -4,11 +4,7 @@ import pandas as pd
 from pydantic import ValidationError
 
 from . import constants
-from .utils.separation import (
-    get_row_index_if_it_belongs_to_device,
-    get_row_index_if_it_belongs_to_drug,
-    separate_dataframe_by_indexes,
-)
+from .utils.separation import separate_dataframe_by_indexes, separate_dataframe_by_product_type
 from .utils.strings import remove_zeros_from_the_beginning
 from .utils.validation import get_indices_and_info_from_errors, insert_row_errors_info_into_df_by_index
 
@@ -16,15 +12,48 @@ try:
     from app.pipiline.utils.base import get_cached_mapping
 except ImportError:
     DRUG_NOMENCLATURE = pd.read_excel('auxiliary_files/Номенклатура_лекарств.xlsx')
+    SERVICES_FROM_FILE = pd.read_excel('auxiliary_files/services_nphies_codes.xlsx')
 else:
     DRUG_NOMENCLATURE = get_cached_mapping('data_preparator_drug_nomenclature')
+    SERVICES_FROM_FILE = get_cached_mapping('data_preparator_services_nphies_codes.xlsx')
 
 
-NPHIES_CODE_WITHOUT_DASHES = 'NPHIES_CODE_WITHOUT_DASHES'
+NPHIES_CODE = 'NPHIES_CODE'
 
 
-def separate_drugs(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """Отделяет от data frame'а строки с лекарствами."""
+def separate_df(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """Разделяет dataframe на услуги, девайсы и лекарства."""
+    df, df_with_services_by_nphies_codes = separate_services_by_nphies_codes(df)
+    df, df_with_medical_devices_by_nphies_codes = separate_devices_by_nphies_codes(df)
+    df, df_with_drugs_by_nphies_codes = separate_drugs_by_nphies_codes(df)
+
+    df, df_with_medical_devices_by_product_type = separate_dataframe_by_product_type(df, 'DEVICES')
+    df, df_with_drugs_by_product_type = separate_dataframe_by_product_type(df, 'MEDS')
+
+    df_with_services = pd.concat((df, df_with_services_by_nphies_codes))
+    df_with_medical_devices = pd.concat(
+        (df_with_medical_devices_by_nphies_codes, df_with_medical_devices_by_product_type),
+    )
+    df_with_drugs = pd.concat((df_with_drugs_by_nphies_codes, df_with_drugs_by_product_type))
+    df_with_services.sort_index(inplace=True)
+    df_with_medical_devices.sort_index(inplace=True)
+    df_with_drugs.sort_index(inplace=True)
+    return df_with_services, df_with_medical_devices, df_with_drugs
+
+
+def separate_services_by_nphies_codes(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Отделяет от data frame'а по НФИС кодам строки с услугами."""
+    nphies_codes_from_file_with_services = SERVICES_FROM_FILE[NPHIES_CODE].values.tolist()
+    indexes_of_rows_with_services = [
+        row_index
+        for row_index, nphies_code in zip(df.index, df[NPHIES_CODE])
+        if nphies_code in nphies_codes_from_file_with_services
+    ]
+    return separate_dataframe_by_indexes(df, indexes_of_rows_with_services)
+
+
+def separate_drugs_by_nphies_codes(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Отделяет от data frame'а по НФИС кодам строки с лекарствами."""
     nphies_codes_of_drugs_in_nomenclature = DRUG_NOMENCLATURE['CODE'].values.tolist()
     nphies_codes_of_drugs_in_nomenclature = [
         remove_zeros_from_the_beginning(nphies_code)
@@ -33,66 +62,63 @@ def separate_drugs(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
 
     nphies_codes_without_zeros = [
         remove_zeros_from_the_beginning(nphies_code.strip())
-        for nphies_code in df['NPHIES_CODE']
+        for nphies_code in df[NPHIES_CODE]
     ]
 
-    nphies_codes_and_product_types_and_indexes = zip(
-        nphies_codes_without_zeros,
-        df['PRODUCT_TYPE'],
-        df.index,
-    )
-
     indexes_of_rows_with_drugs = [
-        get_row_index_if_it_belongs_to_drug(
-            nphies_code,
-            product_type,
-            row_index,
-            nphies_codes_of_drugs_in_nomenclature,
-        )
-        for nphies_code, product_type, row_index in nphies_codes_and_product_types_and_indexes
-    ]
-    indexes_of_rows_with_drugs = [
-        index
-        for index in indexes_of_rows_with_drugs
-        if index is not None
+        row_index
+        for row_index, nphies_code in zip(df.index, nphies_codes_without_zeros)
+        if nphies_code in nphies_codes_of_drugs_in_nomenclature
     ]
 
     df, df_with_drugs = separate_dataframe_by_indexes(df, indexes_of_rows_with_drugs)
-    df_with_drugs['NPHIES_CODE'] = [
+    df_with_drugs[NPHIES_CODE] = [
         remove_zeros_from_the_beginning(nphies_code.strip())
-        for nphies_code in df_with_drugs['NPHIES_CODE']
+        for nphies_code in df_with_drugs[NPHIES_CODE]
     ]
     return df, df_with_drugs
 
 
-def separate_devices(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """Отделяет строки с device'ами в отдельный дата фрейм."""
-    df[NPHIES_CODE_WITHOUT_DASHES] = df['NPHIES_CODE']
-    df[NPHIES_CODE_WITHOUT_DASHES] = df[NPHIES_CODE_WITHOUT_DASHES].apply(
-        lambda nphies_code: nphies_code.replace('-', ''),
-    )
-
-    nphies_codes_and_product_types_and_indexes = zip(
-        df[NPHIES_CODE_WITHOUT_DASHES],
-        df['PRODUCT_TYPE'],
-        df.index,
-    )
-
-    indexes_of_rows_with_devices = [
-        get_row_index_if_it_belongs_to_device(
-            nphies_code,
-            product_type,
-            row_index,
-        )
-        for nphies_code, product_type, row_index in nphies_codes_and_product_types_and_indexes
-    ]
-    indexes_of_rows_with_devices = [
-        index
-        for index in indexes_of_rows_with_devices
-        if index is not None
+def separate_devices_by_nphies_codes(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Отделяет строки с device'ами по длине НФИС кода в отдельный дата фрейм."""
+    nphies_code_without_dashes = [
+        nphies_code.replace('-', '')
+        for nphies_code in df[NPHIES_CODE]
     ]
 
-    df.drop(NPHIES_CODE_WITHOUT_DASHES, axis='columns', inplace=True)
+    indexes_of_rows_with_devices = [
+        row_index
+        for row_index, nphies_code in zip(df.index, nphies_code_without_dashes)
+        if len(nphies_code) == 5
+    ]
+
+    return separate_dataframe_by_indexes(df, indexes_of_rows_with_devices)
+
+
+def separate_drugs_by_product_type(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Отделяет от data frame'а по типу услуги строки с лекарствами."""
+    indexes_of_rows_with_drugs = [
+        row_index
+        for row_index, product_type in zip(df.index, df['PRODUCT_TYPE'])
+        if product_type == 'MEDS'
+    ]
+
+    df, df_with_drugs = separate_dataframe_by_indexes(df, indexes_of_rows_with_drugs)
+    df_with_drugs[NPHIES_CODE] = [
+        remove_zeros_from_the_beginning(nphies_code.strip())
+        for nphies_code in df_with_drugs[NPHIES_CODE]
+    ]
+    return df, df_with_drugs
+
+
+def separate_devices_by_product_type(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Отделяет строки с device'ами по типу в отдельный дата фрейм."""
+    indexes_of_rows_with_devices = [
+        row_index
+        for row_index, product_type in zip(df.index, df['PRODUCT_TYPE'])
+        if product_type == 'DEVICE'
+    ]
+
     return separate_dataframe_by_indexes(df, indexes_of_rows_with_devices)
 
 
--- data_preparator/utils/separation.py
+++ data_preparator/utils/separation.py
@@ -13,27 +13,11 @@ def separate_dataframe_by_indexes(
     return main_df, separated_df
 
 
-def get_row_index_if_it_belongs_to_device(
-    nphies_code_without_dashes: str,
-    product_type: str,
-    index: int,
-) -> int:
-    """Возвращает переданный индекс строки, если строка принадлежит device'у."""
-    if len(nphies_code_without_dashes) == 5:
-        return index
-
-    elif product_type == 'DEVICE':
-        return index
-
-
-def get_row_index_if_it_belongs_to_drug(
-    nphies_code_without_zeros_at_the_beginning: str,
-    product_type: str,
-    index: int,
-    nphies_codes_of_drugs_in_nomenclature,
-) -> int:
-    """Возвращает переданный индекс строки, если строка принадлежит лекарству."""
-    if nphies_code_without_zeros_at_the_beginning in nphies_codes_of_drugs_in_nomenclature:
-        return index
-    elif product_type == 'MEDS':
-        return index
+def separate_dataframe_by_product_type(
+    main_df: pd.DataFrame,
+    product_type_by_which_to_separate: str,
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Отделяет от переданного dataframe'а строки с переданным типом услуги в отдельный dataframe."""
+    separated_df = main_df.loc[main_df['PRODUCT_TYPE'] == product_type_by_which_to_separate]
+    main_df = main_df.loc[~main_df.index.isin(separated_df.index)]
+    return main_df, separated_df
